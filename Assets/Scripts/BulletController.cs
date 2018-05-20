17b00b5659b42aae0f341e08a4e483f45496fbce
rochamario <mario.rocha@msn.com>
2018-05-20 19:49:17 -0300

    My fist commit

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	private Transform bullet;
+	public float speed;
+	public AudioClip destroyEnemy;
+
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		bullet = GetComponent<Transform> ();
+		audioSource = GetComponent<AudioSource>();
+	
+	}
+
+	void FixedUpdate(){
+		bullet.position += Vector3.up * speed;
+
+		if (bullet.position.y >= 10)
+			Destroy (gameObject);
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Enemy") {
+			Destroy (other.gameObject);
+			Destroy (gameObject);
+			audioSource.clip = destroyEnemy;
+			audioSource.Play();
+			PlayerScore	.playerScore += 10;
+							
+		} else if (other.tag == "Base")
+			Destroy (gameObject);
+	}
+
+}
